df0a8adea7ea6738845e0b8ee3e5dea0c204f2d7
Philippe Canal <pcanal@fnal.gov>
2015-02-26 18:33:32 -0600

    Add the ability to unlock early the guarded mutex

--- core/base/inc/TVirtualMutex.h
+++ core/base/inc/TVirtualMutex.h
@@ -77,19 +77,15 @@ private:
 public:
    TLockGuard(TVirtualMutex *mutex)
      : fMutex(mutex) { if (fMutex) fMutex->Lock(); }
-   Int_t UnLock() { 
-      if (!fMutex) return 0;
-      auto tmp = fMutex;
-      fMutex = 0;
-      return tmp->UnLock();
-   }
-   ~TLockGuard() { if (fMutex) fMutex->UnLock(); }
+   void UnLock() { if (fMutex) { fMutex->UnLock(); fMutex = 0; } }
+   virtual ~TLockGuard() { if (fMutex) fMutex->UnLock(); }
 
-   ClassDefNV(TLockGuard,0)  // Exception safe locking/unlocking of mutex
+   ClassDef(TLockGuard,0)  // Exception safe locking/unlocking of mutex
 };
 
 // Zero overhead macros in case not compiled with thread support
 #if defined (_REENTRANT) || defined (WIN32)
+
 #define R__LOCKGUARD(mutex) TLockGuard _R__UNIQUE_(R__guard)(mutex)
 #define R__LOCKGUARD_NAMED(name,mutex) TLockGuard name(mutex)
 #define R__LOCKGUARD2(mutex)                             \
@@ -100,10 +96,14 @@ public:
       gGlobalMutex->UnLock();                            \
    }                                                     \
    R__LOCKGUARD(mutex)
+#define R__LOCKGUARD_NAMED(name,mutex) TLockGuard _NAME2_(R__guard,name)(mutex)
+#define R__LOCKGUARD_UNLOCK(name) _NAME2_(R__guard,name).UnLock()
 #else
 #define R__LOCKGUARD(mutex)  if (mutex) { }
 #define R__LOCKGUARD_NAMED(name,mutex) if (mutex) { }
 #define R__LOCKGUARD2(mutex) if (mutex) { }
+#define R__LOCKGUARD_NAMED(name,mutex) { }
+#define R__LOCKGUARD_UNLOCK(name) { }
 #endif
 
 #endif
