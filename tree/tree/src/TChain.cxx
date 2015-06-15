2503e71620d043e19b74f62cc441420a28727722
Christopher Jones <cdj@fnal.gov>
2015-06-15 17:24:17 +0200

    Use a lock to protect access to collection from TROOT::GetListOfCleanups()
    The protection of the collection returned from TROOT::GetListOfCleanups()
    was insufficient since thread related crashes could still occur. This
    commit protects all uses except for those from the GUI.
    
    (cherry picked from commit b4cee9c49a9fb4839d494323aee30842fd3de0a2)
    
    Signed-off-by: David Abdurachmanov <David.Abdurachmanov@cern.ch>

--- tree/tree/src/TChain.cxx
+++ tree/tree/src/TChain.cxx
@@ -53,6 +53,7 @@ the trees in the chain.
 #include "TEntryListFromFile.h"
 #include "TFileStager.h"
 #include "TFilePrefetch.h"
+#include "TVirtualMutex.h"
 
 ClassImp(TChain)
 
@@ -89,6 +90,7 @@ TChain::TChain()
    gROOT->GetListOfDataSets()->Add(this);
 
    // Make sure we are informed if the TFile is deleted.
+   R__LOCKGUARD2(gROOTMutex);
    gROOT->GetListOfCleanups()->Add(this);
 }
 
@@ -165,6 +167,7 @@ TChain::TChain(const char* name, const char* title)
    gROOT->GetListOfDataSets()->Add(this);
 
    // Make sure we are informed if the TFile is deleted.
+   R__LOCKGUARD2(gROOTMutex);
    gROOT->GetListOfCleanups()->Add(this);
 }
 
@@ -173,7 +176,10 @@ TChain::TChain(const char* name, const char* title)
 
 TChain::~TChain()
 {
-   gROOT->GetListOfCleanups()->Remove(this);
+   {
+      R__LOCKGUARD2(gROOTMutex);
+      gROOT->GetListOfCleanups()->Remove(this);
+   }
 
    SafeDelete(fProofChain);
    fStatus->Delete();
@@ -2807,7 +2813,10 @@ void TChain::Streamer(TBuffer& b)
 {
    if (b.IsReading()) {
       // Remove using the 'old' name.
-      gROOT->GetListOfCleanups()->Remove(this);
+      {
+         R__LOCKGUARD2(gROOTMutex);
+         gROOT->GetListOfCleanups()->Remove(this);
+      }
 
       UInt_t R__s, R__c;
       Version_t R__v = b.ReadVersion(&R__s, &R__c);
@@ -2828,7 +2837,10 @@ void TChain::Streamer(TBuffer& b)
          //====end of old versions
       }
       // Re-add using the new name.
-      gROOT->GetListOfCleanups()->Add(this);
+      {
+         R__LOCKGUARD2(gROOTMutex);
+         gROOT->GetListOfCleanups()->Add(this);
+      }
 
    } else {
       b.WriteClassBuffer(TChain::Class(),this);
