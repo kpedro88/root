dd5bc256d152d35f4df22fce7d4734d951a5901e
David Abdurachmanov <David.Abdurachmanov@cern.ch>
2016-06-16 23:36:32 +0200

    PowerPC (POWER8, ppc64le) support
    - Switch to large code model on PowerPC in Cling, thus allowing larger
      TOC offset.
    
    Details available here:
    https://indico.cern.ch/event/463697/contribution/9/attachments/1193199/1732527/cmsrd.2015.nov.23.pdf
    
    Signed-off-by: David Abdurachmanov <David.Abdurachmanov@cern.ch>

--- interpreter/cling/lib/Interpreter/IncrementalExecutor.cpp
+++ interpreter/cling/lib/Interpreter/IncrementalExecutor.cpp
@@ -89,7 +89,13 @@ std::unique_ptr<TargetMachine>
   std::string FeaturesStr;
 
   TargetOptions Options = TargetOptions();
+  // We have to use large code model for PowerPC because TOC and text sections
+  // can be more than 2GB apart.
+#if defined(__powerpc64__) || defined(__PPC64__) || defined(__powerpc__)
+  CodeModel::Model CMModel = CodeModel::Large;
+#else
   CodeModel::Model CMModel = CodeModel::JITDefault;
+#endif
   CodeGenOpt::Level OptLevel = CodeGenOpt::Default;
   switch (CGOpt.OptimizationLevel) {
     case 0: OptLevel = CodeGenOpt::None; break;
